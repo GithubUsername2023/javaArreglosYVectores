24416e0801465f80c40455a55efa72c5919ab716
Martin Selis <98183961+martinselis1978@users.noreply.github.com>
2023-04-19 22:17:37 -0300

    arreglos y vectores

---
+++ src/arreglosyvectores/Clase4_3.java
@@ -0,0 +1,13 @@
+package arreglosyvectores;
+
+public class Clase4_3 {
+
+    public static void main(String[] args) {
+        /*
+        Recorrer un vector de N enteros contabilizando cuántos números son de
+        1 dígito, cuántos de 2 dígitos, etcétera (hasta 5 dígitos)
+        */
+        
+    }
+    
+}
